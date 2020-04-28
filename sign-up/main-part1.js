ac10371ac362778326cd30d25f703781f0a85608
Cloudy Young <cloudyyoung74@outlook.com>
2020-04-28 00:01:16 -0600

    Create main-part1.js

---
+++ sign-up/main-part1.js
@@ -0,0 +1,269 @@
+'use strict';
+(function(a) {
+  var ret = {
+    "imgsBase" : "https://acctcdn.msauth.net/images/",
+    "clientEvents" : {
+      "enabled" : 1,
+      "url" : "/API/ClientEvents",
+      "flush" : 60000,
+      "autoPost" : 1,
+      "autoPostDelay" : 1000,
+      "minEvents" : 1,
+      "maxEvents" : 1,
+      "pltDelay" : 500
+    },
+    "serverDetails" : {
+      "dc" : "eastus",
+      "ri" : "eusXXXX003A",
+      "ver" : {
+        "v" : [2, 0, 1537, 0]
+      },
+      "rt" : "2020-04-28T05:47:47",
+      "et" : 15
+    },
+    "watson" : {
+      "enabled" : 1,
+      "bundle" : "https://acctcdn.msauth.net/watson_rjZS-jaNNRyqe9ESKNv5iw2.js?v=1",
+      "sbundle" : "https://acctcdn.msauth.net/jquerypackage_1.10_5V7LAuc3bNAQx2QQfr1RPw2.js?v=1",
+      "fbundle" : "https://acctcdn.msauth.net/datarequestpackage_dT3VZJ_4lD5UykUFoE8W2w2.js?v=1",
+      "resetErrorPeriod" : 5,
+      "maxCorsErrors" : 2,
+      "maxInjectErrors" : 5,
+      "maxErrors" : 10,
+      "maxTotalErrors" : 100,
+      "incScripts" : 1,
+      "expSrcs" : ["acctcdn.msauth.net", "acctcdn.msftauth.net", "acctcdnmsftuswe2.azureedge.net", "acctcdnvzeuno.azureedge.net", "account.live.com", "login.live.com"],
+      "expectedVersions" : ["1.10.2"]
+    },
+    "clientTelemetry" : {
+      "uaid" : "05fa7363ea504806a1ba3a01b72e031e",
+      "tcxt" : "t43B/6XkqGqPlM4A7KtnaKL/XZCr2iVgHpX0Wn1gcO9CFIr2+XoNW83odyEtyt4LWvBmuYLbjWzjrBm18xk/iTHS6di5Nw2lFvEbVs/BiXWNClsU2lV+W8dm3Io3In8FT7tXhcSy6s29YFROSrR/1ZeR63FIEXWq0z2D6NxTBI7jsrShJ5jYIfHjxJGMYnwjnA3+bZqR5Tj+2GUfSQrkMXyPuGux6EjRzLsC5xbGEWLK/AgFoGqtfUtVqEA+uRHf7Qi1+0KsCR7uE5t2HJkGrlM0zpilB/zieWyr7KczVi7oX7Ep7bydbeGBahcpRMwvHQAGkOlPuk8lfaNhBXT0skkahb5uZf10y+ebbAV3CUjPZ9IbXboHvLMHpvKxCnMCYuKJkIrVRzI2isR+kVy8XKMx+b/1RNSxbQazEE2FbPw24fvwyGK8cnnKnM99cQM1/72ScUHpeGOWLLOk0AlCKQ==:2:3"
+    },
+    "WLXAccount" : {
+      "Animation" : {
+        "showConvergedAnimation" : 1
+      },
+      "urls" : {
+        "baseDomain" : "https://signup.live.com/",
+        "reportClientData" : "API/ReportClientEvent"
+      },
+      "hasWin10NonOOBEBehavior" : 0,
+      "hasWin10Behavior" : 0,
+      "signup" : {
+        "showConvergedFrame" : 1,
+        "page" : {
+          "isRTL" : 0,
+          "imgs" : {
+            "baseUrl" : "https://acctcdn.msauth.net/images/",
+            "marchingAnts" : "marching_ants_tUCo5RgDcZLjLE_li_Lbqw2.gif",
+            "marchingAntsWhite" : "marching_ants_white_Fm3lNHEmUlOrOkVt7-baIw2.gif",
+            "msLogo" : "microsoft_logo_7lyNn7YkjJOP0NwZNw6QvQ2.svg",
+            "msLogoWhite" : "microsoft_logo_white_WV6SBtQnTELCe2bjcfpjPQ2.svg",
+            "leftArrow" : "Arrows/left_qcwoJO81F7bEFg3Pj_fUEA2.svg",
+            "leftArrowWhite" : "Arrows/left_white_48cQvjBSJTrXyqU8Jwd2gw2.svg",
+            "rightArrow" : "Arrows/right_aGpzma_a9NXwYO6wizIMQg2.svg",
+            "rightArrowWhite" : "Arrows/right_white_BCQ8nqTxlKnILt4pCthDYw2.svg",
+            "dropdownCaret" : "dropdown_caret_KXSZjGsyILZaoTf0sI9X-A2.svg",
+            "dropdownCaretWhite" : "dropdown_caret_white_eYnSFE34p5V81ElMUenxtg2.svg",
+            "arrow" : "Arrows/left_qcwoJO81F7bEFg3Pj_fUEA2.svg",
+            "arrowWhite" : "Arrows/left_white_48cQvjBSJTrXyqU8Jwd2gw2.svg",
+            "accountIcon" : "picker_account_msa_LY-GBZvhdoM4lwme5t3t6w2.svg",
+            "progressIndicatorLarge" : "common/spinner_grey_120_al8Ofe5eGXw1aCTwvKglIg2.gif"
+          },
+          "showDesc" : 0,
+          "showEasiSignupDesc" : 0,
+          "showNewEmailOption" : 1,
+          "credentialsStep" : 1,
+          "passwordStep" : 2,
+          "profileAccrualStep" : 3,
+          "birthDateCountryAccrualStep" : 4,
+          "verificationStep" : 5,
+          "totalStep" : 5,
+          "isPaginated" : 1,
+          "noAuthCancel" : 1,
+          "pwdless" : 0,
+          "pwdlessemail" : 0,
+          "showInlinePhoneCountryCode" : 1,
+          "collectDob" : 1,
+          "isFLOrder" : 1,
+          "shouldSuppressCapsLockWarning" : 0,
+          "useSkypeIcon" : 0,
+          "appCentipedeHtml" : null,
+          "useKoreaPrivacy" : 0,
+          "collectFLName" : 1,
+          "behavioralTelemetryUrl" : "https://client.hip.live.com/?SessionID=2cce14514d964847a3c3a79101fc726c&SiteID=15041&rand=1678040532",
+          "isMojangUpgrade" : 0,
+          "showMojangUpgradeExp" : 0,
+          "showMojangUpgradeSpeedbump" : 0,
+          "useNewValidationBehavior" : 1
+        },
+        "strings" : {
+          "copyrightNotice" : "&copy; 2020 Microsoft",
+          "convergedTouPrivacyText" : '<a target="_blank" rel="noreferrer noopener" id="terms" href="https://www.microsoft.com/en-us/servicesagreement/default.aspx">Terms of Use</a> <a target="_blank" rel="noreferrer noopener" id="privacy" href="https://go.microsoft.com/fwlink/?LinkID=521839">Privacy & Cookies</a>',
+          "usernameRecoverySpeedbumpPageTitle" : "You may already have an account",
+          "usernameRecoverySpeedbumpDesc" : "{0} is already being used with another Microsoft account. You should sign in with that account.",
+          "usernameRecoverySpeedbumpRecoveryLinkUrl" : "https://account.live.com/username/recover?uaid=05fa7363ea504806a1ba3a01b72e031e&bu=https%3a%2f%2fsignup.live.com%2f%3flic%3d1%26username%3dprefillUserName&ru=https%3a%2f%2flogin.live.com%2f%3funr%3d0&uiflavor=Web&uitheme=",
+          "birthdatePageTitle" : "What's your birth date?",
+          "loading" : "Loading...",
+          "pleaseWait" : "Please wait",
+          "cancelButton" : "Cancel",
+          "passwordPageTitle" : "Create a password",
+          "passwordPageDesc" : "Enter the password you would like to use with your account.",
+          "verifyPageTitlePhone" : "Verify phone number",
+          "verifyPageTitleEASI" : "Verify email",
+          "verifyPageDescEASI" : 'Enter the code we sent to <strong>{0}</strong>. If you didn\'t get the email, check your junk folder or <a id="resendCodeLink" href="#">try again</a>.',
+          "usernamePrefillSpeedbumpDesc" : "Looks like you don't have an account with us. We'll create one for you using <strong id='memberNamePrefill'>{0}</strong>.",
+          "smsDisclaimer" : "By providing your phone number, you agree to receive service notifications to your mobile phone. Text messaging rates may apply.",
+          "memberNameDomainAriaLabel" : "User name domain: {0}",
+          "tooltip" : {
+            "firstName" : "Your name will appear to your friends, co-workers, family, and others in the Microsoft services you use.",
+            "lastName" : "Your name will appear to your friends, co-workers, family, and others in the Microsoft services you use.",
+            "memberName" : {
+              "live" : "Create a Microsoft account to get a new email inbox and sign in to all Microsoft services.",
+              "easi" : "Use your favorite email address as your Microsoft account to sign in to all Microsoft services."
+            },
+            "password" : "Passwords must have at least 8 characters and contain at least two of the following: uppercase letters, lowercase letters, numbers, and symbols.",
+            "country" : {
+              "noneu" : "Your country or territory of residence",
+              "eu" : "Privacy for residents of the European Union"
+            },
+            "birthdate" : "Your date of birth helps us provide you with things like age-appropriate settings. We won't display it without your permission.",
+            "phoneNumber" : "Your date of birth helps us provide you with things like age-appropriate settings. We won't display it without your permission.",
+            "alternateEmail" : "Use an address that's different from your Microsoft account. If you forget your password, we'll send password reset info to this address."
+          },
+          "showEmailDomainOptions" : "Show email domain options",
+          "hideEmailDomainOptions" : "Hide email domain options",
+          "emailDomainOptionsMenuAriaLabel" : "Email domain options",
+          "showPasswordLabel" : "Show password",
+          "evictionSpeedbumpTopDesc" : "{0} is already a Microsoft account.",
+          "evictionSpeedbumpMiddleDesc" : 'If you previously created an account with this phone number, <a href="{0}" id="EvictionSignIn">sign in</a> with it.',
+          "evictionSpeedbumpBottomDesc" : 'If you recently got this phone number and don\'t have an account with us, continue to <a href="#" id="iSignupAction">sign up</a>.',
+          "evictionSpeedbumpPageTitle" : "Create account",
+          "evictionSpeedbumpErrorPageDescription" : 'It looks like you already have a Microsoft account. Choose <span id="signInButton">Next</span> to sign in with that account.'
+        },
+        "showUsernameRecoverySpeedbump" : 1,
+        "usernameRecoverySpeedbumpSignInUrl" : "https://login.live.com/?unr=0",
+        "usernameRecoverySpeedbumpRecoveryLink" : 'If you do not remember your username, <a id="iRecoveryLink" href="#">recover it now</a>.',
+        "overrideStrings" : {
+          "touPrivacyText" : 'Choosing <strong>Next</strong> means that you agree to the <a target="_blank" rel="noreferrer noopener" href="https://www.microsoft.com/en-us/servicesagreement/default.aspx" >Microsoft Services Agreement</a> and <a target="_blank" rel="noreferrer noopener" href="https://go.microsoft.com/fwlink/?LinkID=521839">privacy and cookies statement.</a>',
+          "profileAccrualPageTitle" : "What's your name?"
+        },
+        "progressTextTemplate" : "Step {0} of {1}",
+        "displaySignInLinkInMnError" : 0,
+        "isConvergedUX" : 1,
+        "isOtsFlow" : 0,
+        "domains" : ["outlook.com", "hotmail.com"],
+        "defaultDomain" : "outlook.com",
+        "memberNameType" : {
+          "EASI" : "EASI",
+          "Live" : "Live",
+          "Phone" : "Phone",
+          "Unknown" : "Unknown"
+        },
+        "fl" : "Default_EASI",
+        "suggAccType" : "EASI",
+        "suggType" : "Prefer",
+        "dobReq" : 1,
+        "isOptOut" : 0,
+        "showPasswordPeek" : 1,
+        "signUpPageId" : 200646,
+        "signUpHipEnforcementPageId" : 201040,
+        "fss" : {
+          "isFamilyAddMemberFlow" : 0
+        },
+        "autoVerify" : 0,
+        "sendOttAction" : "SignUp",
+        "channel" : {
+          "email" : "Email",
+          "sms" : "SMS"
+        },
+        "resendCodeTimeout" : 6E4,
+        "sendOttTimeout" : 2E4,
+        "viewContext" : {
+          "data" : {
+            "siteId" : "68692",
+            "isRdm" : 0,
+            "prefill" : {
+              "country" : "US",
+              "optinEmailCheckbox" : 1
+            }
+          }
+        },
+        "easiVerifyInline" : 1,
+        "collectDeviceTicket" : 0,
+        "localAccountProvider" : "CXH.IDPS.Local",
+        "isEvictionSpeedbumpEnabled" : 1,
+        "showEvictionErrorPage" : 1,
+        "evictionSigninURL" : "https://login.live.com",
+        "useSameSite" : 1
+      },
+      "isSkype" : 0,
+      "hip" : {
+        "url" : "https://client.hip.live.com/GetHIP/GetHIPAMFE/HIPAMFE?id=15041&mkt=en-US&fid=2cce14514d964847a3c3a79101fc726c&type=visual&rand=1305941566",
+        "fid" : "2cce14514d964847a3c3a79101fc726c",
+        "required" : 0,
+        "enforcement" : {
+          "url" : "https://iframe-auth.arkoselabs.com/B7D8911C-5CC8-A9A3-35B0-554ACEE604DA/index.html?mkt=en",
+          "pid" : "B7D8911C-5CC8-A9A3-35B0-554ACEE604DA"
+        }
+      },
+      "countryCode" : "US",
+      "firstFocusPriorityOrder" : ["#maincontent .has-error,#maincontent .inputState_error", ".notification-body", "#maincontent [autofocus],#maincontent [autofocus=autofocus],#maincontent [autofocus=true],#maincontent input,#maincontent button,#maincontent select,#maincontent a"],
+      "supportsUnifiedHeader" : 0,
+      "convergedFrameConfig" : {
+        "showConvergedFrame" : 1,
+        "page" : {
+          "isRTL" : 0,
+          "imgs" : {
+            "baseUrl" : "https://acctcdn.msauth.net/images/",
+            "marchingAnts" : "marching_ants_tUCo5RgDcZLjLE_li_Lbqw2.gif",
+            "marchingAntsWhite" : "marching_ants_white_Fm3lNHEmUlOrOkVt7-baIw2.gif",
+            "msLogo" : "microsoft_logo_7lyNn7YkjJOP0NwZNw6QvQ2.svg",
+            "msLogoWhite" : "microsoft_logo_white_WV6SBtQnTELCe2bjcfpjPQ2.svg",
+            "leftArrow" : "Arrows/left_qcwoJO81F7bEFg3Pj_fUEA2.svg",
+            "leftArrowWhite" : "Arrows/left_white_48cQvjBSJTrXyqU8Jwd2gw2.svg",
+            "rightArrow" : "Arrows/right_aGpzma_a9NXwYO6wizIMQg2.svg",
+            "rightArrowWhite" : "Arrows/right_white_BCQ8nqTxlKnILt4pCthDYw2.svg",
+            "dropdownCaret" : "dropdown_caret_KXSZjGsyILZaoTf0sI9X-A2.svg",
+            "dropdownCaretWhite" : "dropdown_caret_white_eYnSFE34p5V81ElMUenxtg2.svg",
+            "arrow" : "Arrows/left_qcwoJO81F7bEFg3Pj_fUEA2.svg",
+            "arrowWhite" : "Arrows/left_white_48cQvjBSJTrXyqU8Jwd2gw2.svg",
+            "accountIcon" : "picker_account_msa_LY-GBZvhdoM4lwme5t3t6w2.svg"
+          }
+        },
+        "strings" : {
+          "copyrightNotice" : "&copy; 2020 Microsoft",
+          "convergedTouPrivacyText" : '<a target="_blank" rel="noreferrer noopener" id="terms" href="https://www.microsoft.com/en-us/servicesagreement/default.aspx">Terms of Use</a> <a target="_blank" rel="noreferrer noopener" id="privacy" href="https://go.microsoft.com/fwlink/?LinkID=521839">Privacy & Cookies</a>'
+        }
+      },
+      "disableAnimations" : 1,
+      "useProgressLineIndicator" : 1
+    },
+    "bundle" : {
+      "crossOrigin" : 1
+    },
+    "loader" : {
+      "cdnRoots" : ["https://acctcdn.msauth.net/", "https://acctcdn.msftauth.net/", "https://acctcdnmsftuswe2.azureedge.net/", "https://acctcdnvzeuno.azureedge.net/"]
+    },
+    "scid" : 100118,
+    "hpgid" : 200225,
+    "isCustomPerf" : 1,
+    "uiflvr" : 1001,
+    "btReportingOn" : 0,
+    "cxhReporting" : 1,
+    "CXHMsaBinaryVersion" : 0,
+    "uaid" : "05fa7363ea504806a1ba3a01b72e031e",
+    "uhf" : {
+      "currentMenuItemId" : ""
+    },
+    "apiCanary" : "8VXVq0aQe5Urw/aUuZQWi1MVlzIe3+MiPsurAGGrdokI12terp1KpbkN6qIiIvse5RpVmuDx6Ev7s0WV7hH9F66VykOt3QatEWP0VqwrmHrGhLJpZ0HtREdxNwqejHHx9wGIRHtj/+6XKMuNK04MI49lzzfQeJL3dXza1qxnNLx+D2AAVJFAqWnimAbgre7xDwIrAjOCGA0Dqt3TYLt1LdQPcXR6AChQG3v66I+n4B2XCtWOUqwAo1Zg1pNP7gty:2:3c",
+    "ip" : "66.115.147.209",
+    "cid" : "UnAuth",
+    "displayMemberName" : "UnAuth",
+    "mkt" : "en-US",
+    "email" : "UnAuth",
+    "mktLocale" : "en-US"
+  };
+  a["$Config"] = $setVar(a["$Config"], ret);
+  a.$Do.register("$Config", 0, true);
+})(window);
